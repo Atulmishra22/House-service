a23195927fd6e18a302a670b0e8d76bc18abc66d
Atulmishra22 <matul2264@gmail.com>
2024-11-17 02:08:31 +0530

    adding more function

--- backened/useful_function.py
+++ backened/useful_function.py
@@ -58,5 +58,83 @@ def search_customer_address(address):
     return customers
 
 #search service_request by service name
-def search_sr_servicename(id):
-    service_request = ServiceRequest.query.filter()
+def search_sr_servicename(name):
+    service_id = Service.query.filter_by(name= name).first()
+    service_requests = ServiceRequest.query.filter_by(service_id=service_id).all()
+    return service_requests
+
+def search_sr_professionalname(name):
+    professional_id = Professional.query.filter_by(name= name).first()
+    service_requests = ServiceRequest.query.filter_by(professional_id=professional_id).all()
+    return service_requests
+
+def search_sr_customername(name):
+    customer_id = Customer.query.filter_by(name=name).first()
+    service_requests = ServiceRequest.query.filter_by(customer_id=customer_id).all()
+    return service_requests
+
+def search_sr_status(status):
+    service_requests = ServiceRequest.query.filter_by(service_status=status).all()
+    return service_requests
+
+def data_from_service(param):
+    name = search_service_name(param)
+    price = search_service_price(param)
+    time = search_service_time(param)
+    if name:
+        return name
+    elif price:
+        return price
+    elif time:
+        return time
+    else:
+        return None
+    
+def data_from_professional(param):
+    name = search_professional_name(param)
+    service = search_professional_service_type(param)
+    address = search_professional_address(param)
+    pin = search_professional_pincode(param)
+    status = search_professional_status(param)
+    if name:
+        return name
+    elif service:
+        return service
+    elif address:
+        return address
+    elif pin:
+        return pin
+    elif status:
+        return status
+    else:
+        return None
+    
+def data_from_customer(param):
+    name = search_customer_name(param)
+    address = search_customer_address(param)
+    pin = search_customer_pincode(param)
+    if name:
+        return name
+    elif address:
+        return address  
+    elif pin:
+        return pin
+    else:
+        return None
+    
+def data_from_servicerequest(param):
+    pname = search_sr_professionalname(param)
+    sname = search_sr_servicename(param)
+    cname = search_sr_customername(param)
+    status = search_sr_status(param)
+    if pname:
+        return pname
+    elif sname:
+        return sname
+    elif cname:
+        return cname
+    elif status:
+        return status
+    else:
+        return None
+    
